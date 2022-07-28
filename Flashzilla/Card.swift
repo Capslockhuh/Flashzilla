0195bec2fbd40996f2da3cd71869653c93210761
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-28 08:51:20 +0200

    Added the UI and some basic functionality

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Jan Andrzejewski on 28/07/2022.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
